ffb24d256eceeb19610dfc0c1b86a27aebc5cf9f
Hemerson <hdpabonm@gmail.com>
2024-11-13 14:38:56 -0500

    primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.calculadora {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.calculadora to javafx.fxml;
+    exports org.calculadora;
+}
