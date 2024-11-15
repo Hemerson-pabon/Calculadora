ac6679aae23f61ecaf5a4fdc07f8fa67a17ff5d4
Deivid <deividsantiago7@gmail.com>
2024-11-15 10:02:30 -0500

    mensaje ola

--- src/main/java/org/calculadora/HelloApplication.java
+++ src/main/java/org/calculadora/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    //ola
     public static void main(String[] args) {
         launch();
     }
